a4af6ad092a76acf8defc532f511b083602d9dc9
ashar <ashar@DESKTOP-LIU49OS>
2022-06-17 10:15:12 +0500

    Second Commit

--- src/components/Services/ParcelInfo.js
+++ src/components/Services/ParcelInfo.js
@@ -21,7 +21,7 @@ const Parcel = () => {
 
   function makeid(length) {
     var result           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyzZ0123456789';
     var charactersLength = characters.length;
     for ( var i = 0; i < length; i++ ) {
       result += characters.charAt(Math.floor(Math.random() * 
